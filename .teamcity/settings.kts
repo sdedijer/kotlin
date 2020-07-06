9eeefe84ef709deeddbd3ceb64ecd03e7bde0fd6
Sanja Dedijer <dedijer@puzzle.ch>
2020-07-06 16:09:23 +0200

    add steps to build configuration

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,4 +1,5 @@
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
+import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 
 /*
@@ -33,6 +34,28 @@ project {
 object Build : BuildType({
     name = "Build"
     description = "abc 123"
+
+    id("Build")
+    steps {
+        script {
+            name = "Set version using script"
+            scriptContent = """
+      #!/bin/bash
+      HASH=%build.vcs.number%
+      SHORT_HASH=${"$"}{HASH:0:7}
+      BUILD_COUNTER=%build.counter%
+      BUILD_NUMBER="1.0${"$"}BUILD_COUNTER.${"$"}SHORT_HASH"
+      echo "##teamcity[buildNumber '${"$"}BUILD_NUMBER']"
+      """.trimIndent()
+        }
+        script {
+            name = "build"
+            scriptContent = """
+      mkdir bin
+      echo "built artifact" > bin/compiled.txt
+      """.trimIndent()
+        }
+    }
 
     vcs {
         root(DslContext.settingsRoot)
