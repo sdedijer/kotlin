5da37e9e30f17e3fca1be5609105fe1595ad1505
admin <admin@TeamCity>
2020-07-08 15:09:07 +0000

    TeamCity change in 'Kotlin2' project: Synchronization with own VCS root is enabled

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -1,10 +1,37 @@
-package Kotlin2.buildTypes
-
 import jetbrains.buildServer.configs.kotlin.v2019_2.*
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.script
 import jetbrains.buildServer.configs.kotlin.v2019_2.triggers.vcs
 
-object Kotlin2_Build : BuildType({
+/*
+The settings script is an entry point for defining a TeamCity
+project hierarchy. The script should contain a single call to the
+project() function with a Project instance or an init function as
+an argument.
+
+VcsRoots, BuildTypes, Templates, and subprojects can be
+registered inside the project using the vcsRoot(), buildType(),
+template(), and subProject() methods respectively.
+
+To debug settings scripts in command-line, run the
+
+    mvnDebug org.jetbrains.teamcity:teamcity-configs-maven-plugin:generate
+
+command and attach your debugger to the port 8000.
+
+To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
+-> Tool Windows -> Maven Projects), find the generate task node
+(Plugins -> teamcity-configs -> teamcity-configs:generate), the
+'Debug' option is available in the context menu for the task.
+*/
+
+version = "2020.1"
+
+project {
+
+    buildType(Build)
+}
+
+object Build : BuildType({
     name = "Build"
     description = "Blablabl now push from tc to new branch only 123"
 
@@ -13,7 +40,7 @@ object Kotlin2_Build : BuildType({
     }
 
     vcs {
-        root(Kotlin2.vcsRoots.Kotlin2_HttpsGithubComSdedijerKotlinGitRefsHeadsNewTestBranch)
+        root(DslContext.settingsRoot)
     }
 
     steps {
