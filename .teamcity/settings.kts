152b31743c99829b100ef11ce2f6375793fb47c2
Sanja Dedijer <dedijer@puzzle.ch>
2020-07-08 17:33:22 +0200

    hello world

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -33,7 +33,7 @@ project {
 
 object Build : BuildType({
     name = "Build"
-    description = "abc 123 hello"
+    description = "abc 123 hello world"
 
     params {
         password("variable123",
